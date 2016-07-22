65a7e2661e8f784a685bba5c0b0ea366efc84706
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-07-22 22:14:58 +0100

    1. Fetch Data using Vanilla

--- src/app.js
+++ src/app.js
@@ -3,3 +3,7 @@ let _ = require('lodash');
 
 const fetchUrl = 'http://gateway.marvel.com/v1/public/comics?apikey=fc67721c305c84f50f7c6646c9b8d9d0';
 
+// 1. Fetch the data
+fetch('http://gateway.marvel.com/v1/public/comics?apikey=fc67721c305c84f50f7c6646c9b8d9d0')
+.then((response) => response.json());
+
