6ee256b794a27234b0db2caa5aa5e9a8fea2d1f5
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-07-23 18:15:20 +0100

    4. Display the title and first image for each comic using vanilla

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-var css = require('./app.css');
+let css = require('./app.css');
 let _ = require('lodash');
 
 const fetchUrl = 'http://gateway.marvel.com/v1/public/comics?apikey=fc67721c305c84f50f7c6646c9b8d9d0';
@@ -53,4 +53,30 @@ fetch(fetchUrl)
   const myArrayOfComicsObjects = response.data.results;
   _.map(myArrayOfComicsObjects, 'prices[0].price')
   .filter((price) => price < 4);
+});
+
+// 4. Display the title and first image for each comic
+// Vanilla
+fetch(fetchUrl)
+.then((response) => response.json())
+.then((response) => {
+  const myArrayOfComicsObjects = response.data.results;
+  const comicsList = document.getElementById('comics');
+
+  myArrayOfComicsObjects.forEach((arrayItem) => {
+    const myTitle = arrayItem.title;
+    const comicItem = document.createElement('li');
+    comicsList.appendChild(comicItem);
+    comicItem.innerHTML = JSON.stringify(myTitle);
+
+    const myArrayOfImagesObjects = arrayItem.images;
+    myArrayOfImagesObjects.forEach((arrayItem) => {
+      const myImgPath = arrayItem.path;
+      const myImgExtension = arrayItem.extension;
+      const comicImgUrl = `${myImgPath}.${myImgExtension}`;
+      const comicImgTag = document.createElement('img');
+      comicImgTag.setAttribute('src', comicImgUrl);
+      comicItem.appendChild(comicImgTag);
+    });
+  });
 });
