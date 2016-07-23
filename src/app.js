93a536b7da9421949ff5201c41a02cc63c5b58c0
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-07-23 15:59:05 +0100

    Hygiene, using fetch url variable

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ let _ = require('lodash');
 const fetchUrl = 'http://gateway.marvel.com/v1/public/comics?apikey=fc67721c305c84f50f7c6646c9b8d9d0';
 
 // 1. Fetch the data
-fetch('http://gateway.marvel.com/v1/public/comics?apikey=fc67721c305c84f50f7c6646c9b8d9d0')
+fetch(fetchUrl)
 .then((response) => response.json());
 
 // 2. Filter the data so that it only includes comics with less than 100 pages
 // Vanilla
-fetch('http://gateway.marvel.com/v1/public/comics?apikey=fc67721c305c84f50f7c6646c9b8d9d0')
+fetch(fetchUrl)
 .then((response) => response.json())
 .then((response) => {
   let myArrayOfObj = response.data.results;
