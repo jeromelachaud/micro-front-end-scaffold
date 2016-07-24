973c4b3e0657bcace74e42ac8d302f23656e9dfc
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-07-24 15:23:38 +0100

    6.1. Fetch the data using XMLHttpRequest and Promises

--- src/app.js
+++ src/app.js
@@ -73,3 +73,24 @@ fetch(fetchUrl)
     urlTag.appendChild(imgTag);
   });
 });
+
+// 6.1. Fetch the data using
+// XMLHttpRequest and Promises
+function getFetch(url) {
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+
+    request.onload = () => {
+      if (request.status == 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.StatusText));
+      }
+    };
+
+    request.onerror = () => reject(Error('Network Error'));
+    request.send();
+  });
+}
+getFetch(fetchUrl);
