10571482fe9673ee32003afbefc3d809e20afb37
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-09-02 16:46:12 +0100

    Using property() instead of prop()

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const filterLt100 = _.filter(function(num) {
   return num < 100;
 });
 
-var filterPriceLt40 = _.flowRight([filterLt40, _.map(_.prop('price'))]);
-var filterPagesLt100 = _.flowRight([filterLt100, _.map(_.prop('pages'))]);
+var filterPriceLt40 = _.flowRight([filterLt40, _.map(_.property('price'))]);
+var filterPagesLt100 = _.flowRight([filterLt100, _.map(_.property('pages'))]);
 console.log(filterPriceLt40(comics), 'filterPriceLt40');
 console.log(filterPagesLt100(comics), 'filterPagesLt100');
