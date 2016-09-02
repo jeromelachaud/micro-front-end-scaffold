e3640e320bc405fdf24f96755feb8a8f949ad5bd
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-09-02 15:26:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const _ = require('lodash');
 const __ = require('lodash/fp');
 
-const comics = [{pages: 100,price: 50},{pages: 100,price: 50},{pages: 50, price: 10}];
+const comics = [
+  {pages: 100, price: 50},
+  {pages: 100, price: 50},
+  {pages: 50, price: 10}
+];
 
 const filterLt40 = _.filter(function(num) {
   return num <= 40;
@@ -11,14 +15,7 @@ const filterLt100 = _.filter(function(num) {
   return num < 100;
 });
 
-const filterPriceLt40 = _.flowRight([filterLt40, _.map(__.prop('price'))]);
-const filterPagesLt100 = _.flowRight([filterLt100, _.map(__.prop('pages'))]);
-
-console.log(filterPriceLt40(comics));
-
-
-const filterPrice = _.flowRight(_.map(_.prop('price')));
-
-const filterPages = _.flowRight(_.map(_.prop('pages')));
-
-// console.log(filterPages(comics));
+var filterPriceLt40 = _.flowRight([filterLt40, _.map(_.prop('price'))]);
+var filterPagesLt100 = _.flowRight([filterLt100, _.map(_.prop('pages'))]);
+console.log(filterPriceLt40(comics), 'filterPriceLt40');
+console.log(filterPagesLt100(comics), 'filterPagesLt100');
